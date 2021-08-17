a2c4e4f024bde19162b4fed3d797b12bc2cbb570
Dany Chavez <dany1792dota@gmail.com>
2021-08-16 21:58:46 -0500

    feat:añadimos un archivo de variables

---
+++ DIA1/01-variables.py
@@ -0,0 +1,26 @@
+#Esto es un comentario
+#Variables de texto
+"""
+    Esto es un comentario multilinea
+"""
+
+nombre= "eduardo"
+apellido= 'de rivero'
+
+direccion= "calle O'higgins 125"
+
+texto= """Hola Amigos:
+Buenas noches, les saluda su profe
+                atentamente,
+                    eduardo"""
+
+texto2 = """
+Hola amigos 
+ya viene el break"""
+
+#variables numericas
+año=2021
+estatura= 1.75
+
+print(type(nombre))
+
